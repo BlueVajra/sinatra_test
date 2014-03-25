224e28896d5fc4c6ce0b97014ae89da02aea0cce
Paul Wenig <pwenig@gmail.com>
2014-03-25 13:44:24 -0600

    refactored
    Signed-off-by: Cory Leistikow <cory.leistikow@gmail.com>

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -14,43 +14,28 @@ feature 'manage items' do
     click_on("Add New Item")
     expect(page).to have_title "Add Awesome Item"
 
-    fill_in 'new_item', with:'soup'
+    fill_in 'new_item', with: 'soup'
     click_on('Submit')
-    expect(page).to have_title "Our Awesome Index Page"
-
     expect(page).to have_content "soup"
 
     click_on("Add New Item")
-    fill_in 'new_item', with:'chicken dumplings'
+    fill_in 'new_item', with: 'chicken dumplings'
     click_on('Submit')
 
     click_on("Add New Item")
-    fill_in 'new_item', with:'calamari'
+    fill_in 'new_item', with: 'calamari'
     click_on('Submit')
 
-    #save_and_open_page
-
     click_on("soup")
-    expect(page).to have_title "Item - soup"
     our_field = find_field('edit_item').value
-    expect(our_field ).to eq "soup"
-    fill_in 'edit_item', with:'chicken soup'
+    expect(our_field).to eq "soup"
+    fill_in 'edit_item', with: 'chicken soup'
     click_on('Edit')
     expect(page).to have_content "chicken soup"
-    #save_and_open_page
+
     click_on('calamari')
     click_on('Delete')
-    expect(page).to have_title "Our Awesome Index Page"
     expect(page).to_not have_content "calamari"
-
-    save_and_open_page
-
-  # add more items (3 more)
-  #  check index to see if there
-  #  delete item 2
-  #  check to make sure others are there, but item t is not
-
-
 
   end
 
